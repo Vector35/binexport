8d659ee6738405fb2292107980533f3247a9d684
Jordan Wiens <jordan@psifertex.com>
2025-07-11 01:19:30 -0400

    cross-platform build fixes

--- cmake/FindBinaryNinja.cmake
+++ cmake/FindBinaryNinja.cmake
@@ -11,28 +11,62 @@ endif()
 # Set up binaryninjaapi library
 if(NOT TARGET binaryninjaapi)
     add_library(binaryninjaapi STATIC IMPORTED GLOBAL)
-    set_target_properties(binaryninjaapi PROPERTIES
-        IMPORTED_LOCATION "${BN_API_PATH}/../build/api/out/libbinaryninjaapi.a"
-        INTERFACE_INCLUDE_DIRECTORIES "${BN_API_PATH};${BN_API_PATH}/vendor/fmt/include"
-    )
+    if(WIN32)
+        set_target_properties(binaryninjaapi PROPERTIES
+            IMPORTED_LOCATION "${BN_API_PATH}/../build/api/out/binaryninjaapi.lib"
+            INTERFACE_INCLUDE_DIRECTORIES "${BN_API_PATH};${BN_API_PATH}/vendor/fmt/include"
+        )
+    else()
+        set_target_properties(binaryninjaapi PROPERTIES
+            IMPORTED_LOCATION "${BN_API_PATH}/../build/api/out/libbinaryninjaapi.a"
+            INTERFACE_INCLUDE_DIRECTORIES "${BN_API_PATH};${BN_API_PATH}/vendor/fmt/include"
+        )
+    endif()
 endif()
 
 # Set up binaryninjacore library (use the actual one, not stubs)
 if(NOT TARGET binaryninjacore)
     add_library(binaryninjacore SHARED IMPORTED GLOBAL)
-    set_target_properties(binaryninjacore PROPERTIES
-        IMPORTED_LOCATION "${BN_INSTALL_DIR}/libbinaryninjacore.dylib"
-        IMPORTED_SONAME "@rpath/libbinaryninjacore.1.dylib"
-        INTERFACE_INCLUDE_DIRECTORIES "${BN_API_PATH}"
-    )
+    if(APPLE)
+        set_target_properties(binaryninjacore PROPERTIES
+            IMPORTED_LOCATION "${BN_INSTALL_DIR}/libbinaryninjacore.dylib"
+            IMPORTED_SONAME "@rpath/libbinaryninjacore.1.dylib"
+            INTERFACE_INCLUDE_DIRECTORIES "${BN_API_PATH}"
+        )
+    elseif(UNIX)
+        set_target_properties(binaryninjacore PROPERTIES
+            IMPORTED_LOCATION "${BN_INSTALL_DIR}/libbinaryninjacore.so"
+            IMPORTED_SONAME "libbinaryninjacore.so.1"
+            INTERFACE_INCLUDE_DIRECTORIES "${BN_API_PATH}"
+        )
+    elseif(WIN32)
+        # For Windows, we need to set configuration-agnostic properties
+        set_target_properties(binaryninjacore PROPERTIES
+            IMPORTED_LOCATION_RELEASE "${BN_INSTALL_DIR}/binaryninjacore.dll"
+            IMPORTED_IMPLIB_RELEASE "${BN_INSTALL_DIR}/binaryninjacore.lib"
+            IMPORTED_LOCATION_DEBUG "${BN_INSTALL_DIR}/binaryninjacore.dll"
+            IMPORTED_IMPLIB_DEBUG "${BN_INSTALL_DIR}/binaryninjacore.lib"
+            IMPORTED_LOCATION_RELWITHDEBINFO "${BN_INSTALL_DIR}/binaryninjacore.dll"
+            IMPORTED_IMPLIB_RELWITHDEBINFO "${BN_INSTALL_DIR}/binaryninjacore.lib"
+            IMPORTED_LOCATION_MINSIZEREL "${BN_INSTALL_DIR}/binaryninjacore.dll"
+            IMPORTED_IMPLIB_MINSIZEREL "${BN_INSTALL_DIR}/binaryninjacore.lib"
+            INTERFACE_INCLUDE_DIRECTORIES "${BN_API_PATH}"
+        )
+    endif()
 endif()
 
 # Set up fmt library (needed by Binary Ninja API)
 if(NOT TARGET fmt::fmt)
     add_library(fmt STATIC IMPORTED GLOBAL)
-    set_target_properties(fmt PROPERTIES
-        IMPORTED_LOCATION "${BN_API_PATH}/../build/api/vendor/fmt/libfmt.a"
-    )
+    if(WIN32)
+        set_target_properties(fmt PROPERTIES
+            IMPORTED_LOCATION "${BN_API_PATH}/../build/api/vendor/fmt/fmt.lib"
+        )
+    else()
+        set_target_properties(fmt PROPERTIES
+            IMPORTED_LOCATION "${BN_API_PATH}/../build/api/vendor/fmt/libfmt.a"
+        )
+    endif()
     add_library(fmt::fmt ALIAS fmt)
 endif()
 
